ce28dbe688b5cdc07aa9eeb4197e63034518825b
Ardit Daku <arditdaku1@gmail.com>
2021-08-09 10:56:20 +0200

    Added Banner & Small Cars

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
-import Head from 'next/head'
-import Header from '../components/Header'
+import Head from 'next/head';
+import Banner from '../components/Banner';
+import Header from '../components/Header';
+import SmallCard from '../components/SmallCard';
 
-export default function Home() {
+export default function Home({ exploreData }) {
     return (
         <div className="">
             <Head>
@@ -11,6 +13,37 @@ export default function Home() {
             {/* Header */}
             <Header />
             {/* Banner */}
+            <Banner />
+            <main className="max-w-7xl mx-auto px-8 sm:px-16">
+                <section className="pt-6">
+                    <h2 className="text-4xl font-semibold pb-5">
+                        Explore Nearby
+                    </h2>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                        {/* Pull some data from a server - API -endpints  */}
+                        {exploreData?.map(({ img, distance, location }) => (
+                            <SmallCard
+                                key={img}
+                                image={img}
+                                location={location}
+                                distance={distance}
+                            />
+                        ))}
+                    </div>
+                </section>
+            </main>
         </div>
-    )
+    );
+}
+
+export async function getStaticProps() {
+    const exploreData = await fetch('https://links.papareact.com/pyp').then(
+        (res) => res.json()
+    );
+
+    return {
+        props: {
+            exploreData,
+        },
+    };
 }
