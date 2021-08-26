bb78a0a973b0adb222e442191bfa8ed52f8f75cd
Ardit Daku <arditdaku1@gmail.com>
2021-08-26 13:54:48 +0000

    QF

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-4 space-y-10 md:space-x-0  lg:grid-cols-5 px-32 py-14 bg-gray-100">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 md:space-x-0  lg:grid-cols-5 px-32 py-14 bg-gray-100">
             <div className="space-y-4 text-xs text-gray-800">
                 <h5 className="font-bold">ABOUT</h5>
                 <p>How Airbnb Works</p>
